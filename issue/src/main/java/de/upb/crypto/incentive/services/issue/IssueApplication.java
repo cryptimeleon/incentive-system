f2d7095e79efd7441f486010b032ba5adf69d039
Paul Kramer <pkramer1503@gmail.com>
2020-12-10 15:00:06 +0100

    Create initial spring boot project for issue service.

---
+++ issue/src/main/java/de/upb/crypto/incentive/services/issue/IssueApplication.java
@@ -0,0 +1,13 @@
+package de.upb.crypto.incentive.services.issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueApplication.class, args);
+	}
+
+}
