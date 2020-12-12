941f63a3494ec259a6a4e71fdc09bae186af7c51
Paul Kramer <pkramer1503@gmail.com>
2020-12-12 14:25:31 +0100

    Add exception to api and update swagger documentation.

--- issue/src/main/java/de/upb/crypto/incentive/services/issue/IssueController.java
+++ issue/src/main/java/de/upb/crypto/incentive/services/issue/IssueController.java
@@ -3,10 +3,15 @@ package de.upb.crypto.incentive.services.issue;
 import de.upb.crypto.incentive.procotols.issue.IssueHelper;
 import de.upb.crypto.incentive.procotols.issue.IssueRequest;
 import de.upb.crypto.incentive.procotols.issue.IssueResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class IssueController {
@@ -14,10 +19,21 @@ public class IssueController {
     IssueHelper issueHelper = new IssueHelper();
 
     @GetMapping("/issue")
-    public IssueResponse greeting(@Validated @ModelAttribute IssueRequest request) {
+    @ApiOperation(value = "Issuing protocol", notes = "Issue a new incentive token.", response = IssueResponse.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = IssueResponse.class),
+            @ApiResponse(code = 403, message = "Invalid Issuing Request", response = String.class)
+    })
+    public ResponseEntity<IssueResponse> greeting(@Validated @ModelAttribute IssueRequest request) throws IncentiveException {
         if (request.equals(issueHelper.validRequest)) {
-            return issueHelper.validResponse;
+            return new ResponseEntity<>(issueHelper.validResponse, HttpStatus.OK);
         }
-        return null;
+        throw new IncentiveException();
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(IncentiveException.class)
+    public String handleIncentiveException(IncentiveException ex) {
+        return "An incentive exception occurred!";
     }
 }
