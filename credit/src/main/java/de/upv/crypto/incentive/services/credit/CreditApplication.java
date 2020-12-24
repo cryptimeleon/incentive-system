6006e2c5b348d75a7cdbb5ec1eba21646ebe59fe
Paul Kramer <pkramer1503@gmail.com>
2020-12-24 11:30:57 +0100

    Add credit service.

---
+++ credit/src/main/java/de/upv/crypto/incentive/services/credit/CreditApplication.java
@@ -0,0 +1,12 @@
+package de.upv.crypto.incentive.services.credit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditApplication.class, args);
+	}
+}
---
+++ credit/src/test/java/de/upv/crypto/incentive/services/credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package de.upv.crypto.incentive.services.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
