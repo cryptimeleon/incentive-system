4c3b41702d7468bcca592710c24048f77b25ce9e
Paul Kramer <pkramer1503@gmail.com>
2021-01-18 15:21:19 +0100

    Add basket server project.

---
+++ basketserver/src/test/java/de/upb/crypto/basketserver/BasketserverApplicationTests.java
@@ -0,0 +1,13 @@
+package de.upb.crypto.basketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
