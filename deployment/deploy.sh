a84eebb640651298ba94ed8be430e3f874e09f63
Paul Kramer <pkramer1503@gmail.com>
2021-01-18 16:09:42 +0100

    Add basketserver to deploy script.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -66,8 +66,10 @@ set -e
 ./gradlew clean build
 ./gradlew ":credit:bootBuildImage"
 ./gradlew ":issue:bootBuildImage"
+./gradlew ":basketserver:bootBuildImage"
 kind load docker-image upbcuk/incentive-service-issue --name kind-t2
 kind load docker-image upbcuk/incentive-service-credit --name kind-t2
+kind load docker-image upbcuk/incentive-service-basketserver --name kind-t2
 set +e
 
 # Print available images
