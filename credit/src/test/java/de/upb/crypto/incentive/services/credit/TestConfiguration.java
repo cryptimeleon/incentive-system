80e8b9bdf920bf326a7066616d29608679a5a6fa
Paul Kramer <pkramer1503@gmail.com>
2021-02-02 17:12:50 +0100

    Add configuration that injects test mocks

---
+++ credit/src/test/java/de/upb/crypto/incentive/services/credit/TestConfiguration.java
@@ -0,0 +1,18 @@
+package de.upb.crypto.incentive.services.credit;
+
+import de.upb.crypto.incentive.cryptoprotocol.interfaces.provider.CreditInterface;
+import de.upb.crypto.incentive.services.credit.model.interfaces.BasketServerClientInterface;
+import org.springframework.context.annotation.Bean;
+
+public class TestConfiguration {
+
+    @Bean
+    BasketServerClientInterface basketServerClientInterface() {
+        return new TestBasketServerClientMock();
+    }
+
+    @Bean
+    CreditInterface creditInterface() {
+        return new TestCryptoCreditMock();
+    }
+}
