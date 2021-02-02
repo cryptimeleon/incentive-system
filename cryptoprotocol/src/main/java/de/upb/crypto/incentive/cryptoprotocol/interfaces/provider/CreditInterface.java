f13ec11eb52344dd24c4452743c2c6a4436d9bb4
Paul Kramer <pkramer1503@gmail.com>
2021-02-02 18:36:04 +0100

    Fix typo in interface

--- cryptoprotocol/src/main/java/de/upb/crypto/incentive/cryptoprotocol/interfaces/provider/CreditInterface.java
+++ cryptoprotocol/src/main/java/de/upb/crypto/incentive/cryptoprotocol/interfaces/provider/CreditInterface.java
@@ -4,5 +4,5 @@ public interface CreditInterface {
     // pp, pk, sk are provided via constructor of the class implementing this interface
 
     // earnAmount (k) should be trusted / verified
-    String computeSerializedReponse(String serializedEarnRequest, long earnAmount);
+    String computeSerializedResponse(String serializedEarnRequest, long earnAmount);
 }
