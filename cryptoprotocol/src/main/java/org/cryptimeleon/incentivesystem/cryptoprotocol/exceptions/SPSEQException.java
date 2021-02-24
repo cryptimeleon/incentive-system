3153fde2d2a7d1f8ea0ed09f8506080cea45795f
Paul Kramer <pkramer1503@gmail.com>
2021-02-24 13:40:12 +0100

    Remove unused exception

--- cryptoprotocol/src/main/java/org/cryptimeleon/incentivesystem/cryptoprotocol/exceptions/SPSEQException.java
+++
@@ -1,7 +0,0 @@
-package org.cryptimeleon.incentivesystem.cryptoprotocol.exceptions;
-
-public class SPSEQException extends RuntimeException {
-    public SPSEQException(String message) {
-        super(message);
-    }
-}
