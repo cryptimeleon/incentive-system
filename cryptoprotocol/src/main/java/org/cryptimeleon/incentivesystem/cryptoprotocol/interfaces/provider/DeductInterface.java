a95f7eeae3f38f5ad71297993654f2ffac6ee02f
Paul Kramer <pkramer1503@gmail.com>
2021-02-24 12:53:19 +0100

    Remove obsolete files

--- cryptoprotocol/src/main/java/org/cryptimeleon/incentivesystem/cryptoprotocol/interfaces/provider/DeductInterface.java
+++
@@ -1,8 +0,0 @@
-package org.cryptimeleon.incentivesystem.cryptoprotocol.interfaces.provider;
-
-public interface DeductInterface {
-    // pp, pk, sk are provided via constructor of the class implementing this interface
-
-    // spendAmount should be verified
-    String computeSerializedDeductReponse(String serializedSpendRequest, String serializedDSID, String serializedTID, long spendAmount);
-}
