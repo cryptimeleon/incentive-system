3510e2a4d312617bdf4710162dd6456f2f03467b
Paul Kramer <pkramer1503@gmail.com>
2021-03-17 12:17:30 +0100

    Add earnRequest data object

---
+++ cryptoprotocol/src/main/java/org/cryptimeleon/incentivesystem/cryptoprotocol/model/EarnRequest.java
@@ -0,0 +1,34 @@
+package org.cryptimeleon.incentivesystem.cryptoprotocol.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import org.cryptimeleon.craco.sig.sps.eq.SPSEQSignature;
+import org.cryptimeleon.math.serialization.Representable;
+import org.cryptimeleon.math.serialization.Representation;
+import org.cryptimeleon.math.serialization.annotations.ReprUtil;
+import org.cryptimeleon.math.serialization.annotations.Represented;
+import org.cryptimeleon.math.structures.groups.cartesian.GroupElementVector;
+
+@Value
+@AllArgsConstructor
+public class EarnRequest implements Representable {
+    @NonFinal
+    @Represented(restorer = "SPSEQ")
+    SPSEQSignature blindedSignature;
+
+    @NonFinal
+    @Represented(restorer = "G1")
+    GroupElementVector blindedC;
+
+    @Override
+    public Representation getRepresentation() {
+        return ReprUtil.serialize(this);
+    }
+
+    public EarnRequest(Representation repr, IncentivePublicParameters pp) {
+        new ReprUtil(this)
+                .register(pp.getBg().getG1(), "G1")
+                .register(pp.getSpsEq(), "SPSEQ");
+    }
+}
