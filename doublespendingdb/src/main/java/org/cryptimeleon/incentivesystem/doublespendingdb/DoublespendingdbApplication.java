ba05358d6fe3fe3d1d66a0a71e63dc17f4881cca
Patrick Schürmann <pschuerm@mail.upb.de>
2021-05-28 15:51:18 +0200

    initial commit for doublespendingdb project

---
+++ doublespendingdb/src/main/java/org/cryptimeleon/incentivesystem/doublespendingdb/DoublespendingdbApplication.java
@@ -0,0 +1,13 @@
+package org.cryptimeleon.incentivesystem.doublespendingdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoublespendingdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoublespendingdbApplication.class, args);
+	}
+
+}
---
+++ doublespendingdb/src/test/java/org/cryptimeleon/incentivesystem/doublespendingdb/DoublespendingdbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cryptimeleon.incentivesystem.doublespendingdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoublespendingdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
