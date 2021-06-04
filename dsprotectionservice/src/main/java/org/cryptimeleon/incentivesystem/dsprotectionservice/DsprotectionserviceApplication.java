190d39521a12f6ad7465115bf731d25daf9a4abb
Patrick Schürmann <pschuerm@mail.upb.de>
2021-06-04 14:03:03 +0200

    re-initialized double-spending protection db project

---
+++ dsprotectionservice/src/main/java/org/cryptimeleon/incentivesystem/dsprotectionservice/DsprotectionserviceApplication.java
@@ -0,0 +1,13 @@
+package org.cryptimeleon.incentivesystem.dsprotectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsprotectionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsprotectionserviceApplication.class, args);
+	}
+
+}
