ead2fc7e1a44972b84030def9df49957b6a2e6af
Paul Kramer <pkramer1503@gmail.com>
2021-06-07 21:52:14 +0200

    Add simple info service that exposes pp and provider keys.

---
+++ info/src/main/java/org/cryptimeleon/incentivesystem/services/info/InfoController.java
@@ -0,0 +1,43 @@
+package org.cryptimeleon.incentivesystem.services.info;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@Slf4j
+public class InfoController {
+
+    private InfoService infoService;  // Automatically injects an instance of the service
+
+    /*
+     * Endpoint for alive testing etc.
+     */
+    @GetMapping("/")
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<>("Info Service", HttpStatus.OK);
+    }
+
+    @GetMapping("/public-parameters")
+    public ResponseEntity<String> queryPublicParameters() {
+        return new ResponseEntity<>(infoService.getSerializedPublicParameters(), HttpStatus.OK);
+    }
+
+    @GetMapping("/provider-public-key")
+    public ResponseEntity<String> queryProviderPublicKey() {
+        return new ResponseEntity<>(infoService.getSerializedProviderPublicKey(), HttpStatus.OK);
+    }
+
+    @GetMapping("/provider-secret-key")
+    public ResponseEntity<String> queryProviderSecretKey(@RequestHeader(name = "shared-secret") String providerSharedSecret) {
+        if (infoService.verifyProviderSharedSecret(providerSharedSecret)) {
+            return new ResponseEntity<>(infoService.getSerializedProviderSecretKey(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("", HttpStatus.UNAUTHORIZED);
+    }
+}
---
+++ info/src/test/java/org/cryptimeleon/incentivesystem/services/info/InfoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cryptimeleon.incentivesystem.services.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
