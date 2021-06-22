8600205447eceebd7fed5cd311bdab2aa7dc9676
Paul Kramer <pkramer1503@gmail.com>
2021-06-22 15:07:29 +0200

    Add docker-compose file name to deploy script since it is not default anymore.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,4 +5,4 @@ pushd deployment
 export SHARED_SECRET=$(./util.sh)
 export BASKET_REDEEM_SECRET=$(./util.sh)
 export BASKET_PAY_SECRET=$(./util.sh)
-docker-compose up -d
+docker-compose -f docker-compose-nginx.yaml up -d
