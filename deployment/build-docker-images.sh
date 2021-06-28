8183ac752ce8c60385fa9b404a6b67ea9cd2f385
Paul Kramer <pkramer1503@gmail.com>
2021-06-28 21:36:40 +0200

    Add custom docker image with precompiled mcl for info service.

--- deployment/build-docker-images.sh
+++ deployment/build-docker-images.sh
@@ -5,5 +5,11 @@ set -e
 ./gradlew clean build
 ./gradlew ":services:credit:bootBuildImage"
 ./gradlew ":services:issue:bootBuildImage"
-./gradlew ":services:info:bootBuildImage"
 ./gradlew ":services:basket:bootBuildImage"
+
+# ./gradlew ":services:info:bootBuildImage"
+# Manually build info service with mcl
+pushd ..
+mkdir -p services/info/build/dependency && (cd services/info/build/dependency; jar -xf ../libs/*.jar)
+docker build --build-arg DEPENDENCY=services/info/build/dependency -t cryptimeleon/info-service -f services/info/Dockerfile .
+popd
