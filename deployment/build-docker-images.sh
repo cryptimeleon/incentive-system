8edbcbeff08b7375cac6f1d9e4aae3a46e7f69f3
Paul Kramer <pkramer1503@gmail.com>
2021-06-28 22:19:19 +0200

    Use custom docker container to provide mcl to services

--- deployment/build-docker-images.sh
+++ deployment/build-docker-images.sh
@@ -2,14 +2,39 @@
 
 set -e
 
-./gradlew clean build
-./gradlew ":services:credit:bootBuildImage"
-./gradlew ":services:issue:bootBuildImage"
-./gradlew ":services:basket:bootBuildImage"
+#./gradlew clean build
 
-# ./gradlew ":services:info:bootBuildImage"
+# Read current version from version file
+VERSION=$(cat ./services/version)
+
 # Manually build info service with mcl
-pushd ..
-mkdir -p services/info/build/dependency && (cd services/info/build/dependency; jar -xf ../libs/*.jar)
-docker build --build-arg DEPENDENCY=services/info/build/dependency -t cryptimeleon/info-service -f services/info/Dockerfile .
-popd
+
+# info service
+DEPENDENCY_PATH=services/info/build/dependency
+mkdir -p $DEPENDENCY_PATH && (cd $DEPENDENCY_PATH; jar -xf ../libs/*.jar)
+docker build \
+  --build-arg DEPENDENCY=$DEPENDENCY_PATH \
+  --build-arg APPLICATION=org.cryptimeleon.incentive.services.info.InfoApplication \
+  -t cryptimeleon/incentive-service-info:$VERSION \
+  -f services/Dockerfile .
+
+# issue service
+DEPENDENCY_PATH=services/issue/build/dependency
+mkdir -p $DEPENDENCY_PATH && (cd $DEPENDENCY_PATH; jar -xf ../libs/*.jar)
+docker build \
+  --build-arg DEPENDENCY=$DEPENDENCY_PATH \
+  --build-arg APPLICATION=org.cryptimeleon.incentive.services.issue.IssueApplication \
+  -t cryptimeleon/incentive-service-issue:$VERSION \
+  -f services/Dockerfile .
+
+# credit service
+DEPENDENCY_PATH=services/credit/build/dependency
+mkdir -p $DEPENDENCY_PATH && (cd $DEPENDENCY_PATH; jar -xf ../libs/*.jar)
+docker build \
+  --build-arg DEPENDENCY=$DEPENDENCY_PATH \
+  --build-arg APPLICATION=org.cryptimeleon.incentive.services.credit.CreditApplication \
+  -t cryptimeleon/incentive-service-credit:$VERSION \
+  -f services/Dockerfile .
+
+# basket server does not require mcl
+./gradlew ":services:basket:bootBuildImage"
