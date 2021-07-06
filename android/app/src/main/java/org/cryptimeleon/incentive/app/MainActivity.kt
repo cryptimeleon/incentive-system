d91fa5faa45dc08989f143c522b8399e1b5c6823
Paul Kramer <pkramer1503@gmail.com>
2021-07-06 21:46:56 +0200

    Add bottom navigation as main navigation

--- android/app/src/main/java/org/cryptimeleon/incentive/app/MainActivity.kt
+++ android/app/src/main/java/org/cryptimeleon/incentive/app/MainActivity.kt
@@ -1,22 +1,38 @@
 package org.cryptimeleon.incentive.app
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
-import androidx.navigation.ui.onNavDestinationSelected
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import org.cryptimeleon.incentive.app.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
-    }
+    lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        // Setup NavBar with navigation, set these as top level entities (no up button)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.dashboardFragment, R.id.benchmark_fragment, R.id.settingsFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Connect bottom navigation ids to fragment ids from the navigation
+        binding.bottomNavigation.setupWithNavController(navController)
 
         // Load mcl
         System.loadLibrary("mcljava")
@@ -26,4 +42,12 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
+    fun setBottomNavigationVisibility(visibility: Int) {
+        // get the reference of the bottomNavigationView and set the visibility.
+        binding.bottomNavigation.visibility = visibility
+    }
 }
