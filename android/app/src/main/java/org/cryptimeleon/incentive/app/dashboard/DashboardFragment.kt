c5467b4d500672fb9e55af6454e6bf048d3e03cb
Paul Kramer <pkramer1503@gmail.com>
2021-07-06 21:56:30 +0200

    Refactoring

--- android/app/src/main/java/org/cryptimeleon/incentive/app/dashboard/DashboardFragment.kt
+++ android/app/src/main/java/org/cryptimeleon/incentive/app/dashboard/DashboardFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import org.cryptimeleon.incentive.app.BaseFragment
