5aaae33dbe7b28837e9d020fc8ec0370efbc8525
Paul Kramer <pkramer1503@gmail.com>
2021-07-06 21:53:47 +0200

    Add repository with room database for the cryptographic assets, move setup process to repository

---
+++ android/app/src/main/java/org/cryptimeleon/incentive/app/repository/crypto/Token.kt
@@ -0,0 +1,11 @@
+package org.cryptimeleon.incentive.app.repository.crypto
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tokens")
+data class Token(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "serialized_token") val serializedToken: String
+)
