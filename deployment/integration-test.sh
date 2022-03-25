49dee12bc898e38a588da47c5a9e973fbd593ad7
Paul Kramer <pkramer1503@gmail.com>
2022-03-25 15:29:23 +0100

    Add instructions on how to run integration tests and comments

--- deployment/integration-test.sh
+++ deployment/integration-test.sh
@@ -1,6 +1,21 @@
+#!/bin/bash
+
+# Make sure the script is executed from the root directory
+[[ -e ./deployment/build-docker-images.sh ]] || {
+  printf "Please cd into the root folder of the directory before running this script:\n   ...$ ./deployment/integration-test.sh\n"
+  exit 1
+}
+
+# Build and start services
 ./deployment/build-docker-images.sh
 docker-compose -f ./deployment/docker-compose-localhost.yaml up -d
-pushd services 
 sleep 20
-./gradlew integrationtest
 
+# Run Tests
+pushd services
+./gradlew integrationtest
+popd
+
+# Shutdown services
+# It might be useful to comment this out when working on the client-side code
+docker-compose -f ./deployment/docker-compose-localhost.yaml down
