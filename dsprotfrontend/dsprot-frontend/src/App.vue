b6affd003b61fcbd08329ae540e08541bb8fcce4
Patrick Schürmann <pschuerm@mail.upb.de>
2022-05-31 12:00:12 +0200

    re-created vue project, Header and Footer

---
+++ dsprotfrontend/dsprot-frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Header title="Cryptimeleon Incentive System Double-spending Protection Service" />
+
+  <Footer />
+</template>
+
+<script>
+  import Header from "./components/Header.vue";
+  import Footer from "./components/Footer.vue";
+
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Footer
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ dsprotfrontend/dsprot-frontend/src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer>
+        <p>Cryptimeleon (C) 2020-2022</p>
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        margin-top: 30px;
+        text-align: center;
+    }
+</style>
