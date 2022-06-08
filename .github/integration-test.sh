0a0d86b7b85506ae17cdb05c68964316cb459737
Paul Kramer <pkramer1503@gmail.com>
2022-06-08 19:39:23 +0200

    Add faster integration test

---
+++ .github/integration-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+JARS=(info basket inc)
+
+function start_services() {
+  # shellcheck disable=SC2164
+  pushd services
+  for JAR in "${JARS[@]}"; do
+    java -jar $JAR/build/libs/$JAR.jar &
+  done
+}
+
+function kill_services() {
+  for JAR in "${JARS[@]}"; do
+    PID=$(pgrep -f $JAR.jar)
+    if test ! -z "$PID"; then
+      kill -- $PID
+    fi
+  done
+}
+
+kill_services
+start_services
+sleep 15
+
+./gradlew :client:integrationTest
+
+kill_services
+wait
