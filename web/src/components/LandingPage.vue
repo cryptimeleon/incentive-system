f14d95a17367c50bfcb9e038e88d339a0c1c2cc9
Paul Kramer <pkramer1503@gmail.com>
2022-06-10 15:20:43 +0200

    Remove default start page and add empty landing page

---
+++ web/src/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto">
+    <p class="text-3xl font-medium">
+      Cryptimeleon Incentive System
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
