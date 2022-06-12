be7a22e2121e5efbea4ba18b0c554a1941898a4e
Paul Kramer <pkramer1503@gmail.com>
2022-06-12 22:59:04 +0200

    Add some notes to landing page

--- web/src/components/LandingPage.vue
+++ web/src/components/LandingPage.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-  </div>
+  <p class="text-2xl py-2">About This Project</p>
+  <ul class="list-disc list-outside ml-6">
+    <li>
+      <a href="https://sfb901.uni-paderborn.de/">SFB901</a>
+    </li>
+    <li>
+      Open Source Prototype (<a
+        href="https://github.com/cryptimeleon/incentive-system">cryptimeleon/incentive-system</a>)
+    </li>
+    <li>
+      Powered by the <a href="https://cryptimeleon.org/">Cryptimeleon</a> cryptography prototyping library.
+    </li>
+    <li>
+      Based on the paper <a href=" https://eprint.iacr.org/2020/382">'Privacy-Preserving Incentive Systems with Highly
+      Efficient
+      Point-Collection'</a> by Jan Bobolz, Fabian Eidens, Stephan Krenn, Daniel Slamanig, and Christoph Striecks.
+    </li>
+  </ul>
+
 </template>
 
 <script setup>
