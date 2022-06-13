8747612110d0bbb2c2d7808173e9af7956f5c8a2
Paul Kramer <pkramer1503@gmail.com>
2022-06-13 23:29:22 +0200

    Start building a landing page

--- web/src/components/LandingPage.vue
+++ web/src/components/LandingPage.vue
@@ -1,27 +1,33 @@
 <template>
-  <p class="text-2xl py-2">About This Project</p>
-  <ul class="list-disc list-outside ml-6">
-    <li>
-      <a href="https://sfb901.uni-paderborn.de/">SFB901</a>
-    </li>
-    <li>
-      Open Source Prototype (<a
-        href="https://github.com/cryptimeleon/incentive-system">cryptimeleon/incentive-system</a>)
-    </li>
-    <li>
-      Powered by the <a href="https://cryptimeleon.org/">Cryptimeleon</a> cryptography prototyping library.
-    </li>
-    <li>
-      Based on the paper <a href=" https://eprint.iacr.org/2020/382">'Privacy-Preserving Incentive Systems with Highly
-      Efficient
-      Point-Collection'</a> by Jan Bobolz, Fabian Eidens, Stephan Krenn, Daniel Slamanig, and Christoph Striecks.
-    </li>
-  </ul>
-
+  <div class="prose prose-slate mx-auto">
+    <h1>About This Project</h1>
+    <p>
+      This project page is still under construction!
+    </p>
+    <ul>
+      <li>
+        <a href="https://sfb901.uni-paderborn.de/">SFB901</a>
+      </li>
+      <li>
+        Open Source Prototype (<a
+          href="https://github.com/cryptimeleon/incentive-system">cryptimeleon/incentive-system</a>)
+      </li>
+      <li>
+        Powered by the <a href="https://cryptimeleon.org/">Cryptimeleon</a> cryptography prototyping library.
+      </li>
+      <li>
+        Based on the paper <a href=" https://eprint.iacr.org/2020/382">'Privacy-Preserving Incentive Systems with Highly
+        Efficient
+        Point-Collection'</a> by Jan Bobolz, Fabian Eidens, Stephan Krenn, Daniel Slamanig, and Christoph Striecks.
+      </li>
+      <li>
+        Developed at Paderborn University
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
