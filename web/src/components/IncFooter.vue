0425bab2619d6780aa1b76fe59add24f868fbdfc
Paul Kramer <pkramer1503@gmail.com>
2022-06-13 23:28:54 +0200

    Add minimal footer

---
+++ web/src/components/IncFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="bg-gray-900 text-white">
+    <div class="text-center p-4">
+      2022 Cryptimeleon | <a class="text-white hover:underline" href="#">Privacy Policy</a>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
