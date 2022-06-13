010f99c3d7be8c5b7ea1c63cc317dd80a913b257
Paul Kramer <pkramer1503@gmail.com>
2022-06-13 23:29:06 +0200

    Add minimal header

--- web/src/components/IncHeader.vue
+++ web/src/components/IncHeader.vue
@@ -1,15 +1,7 @@
 <template>
-  <div class="w-full bg-sky-900 text-white shadow mb-8">
-    <nav class="lg:container lg:mx-auto lg:flex lg:justify-between lg:items-center lg:px-16">
-      <p class="text-2xl font-lg py-4">Cryptimeleon Incentive System</p>
-      <ul class="flex-col py-2 lg:py-0 lg:flex lg:flex-row lg:space-x-16">
-        <li>
-          Item 1
-        </li>
-        <li>
-          Item 2
-        </li>
-      </ul>
+  <div class="bg-sky-700 text-slate-100 shadow-lg mb-8 text-sm">
+    <nav class="px-4">
+      <p class="text-xl font-semibold py-4">Cryptimeleon Incentive System</p>
     </nav>
   </div>
 </template>
