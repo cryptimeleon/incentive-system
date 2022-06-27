719f873991960a776f00b1d7c3c6a2dcfc4a6154
Paul Kramer <pkramer1503@gmail.com>
2022-06-27 23:42:40 +0200

    Migrate bottom app navigation

--- android/app/src/main/java/org/cryptimeleon/incentive/app/MainActivity.kt
+++ android/app/src/main/java/org/cryptimeleon/incentive/app/MainActivity.kt
@@ -4,36 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.QrCodeScanner
 import androidx.compose.material.icons.filled.ShoppingBasket
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.insets.LocalWindowInsets
-import com.google.accompanist.insets.rememberInsetsPaddingValues
-import com.google.accompanist.insets.ui.BottomNavigation
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import org.cryptimeleon.incentive.app.theme.CryptimeleonTheme
 import timber.log.Timber
+import kotlin.math.ln
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -52,11 +59,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             // Update the system bars to be translucent
             val systemUiController = rememberSystemUiController()
-            val useDarkIcons = MaterialTheme.colors.isLight
             SideEffect {
                 systemUiController.setSystemBarsColor(
                     Color.Transparent,
-                    darkIcons = useDarkIcons// depends on what background color is used
+                    darkIcons = true // depends on what background color is used
                 )
             }
 
@@ -96,38 +102,43 @@ class MainActivity : ComponentActivity() {
 
         // Only display bottom navigation on mainNavigation Routes
         if (currentRoute in mainNavigationRoutes) {
-            BottomNavigation(
-                contentPadding = rememberInsetsPaddingValues(
-                    LocalWindowInsets.current.navigationBars
-                ),
-            ) {
-                val currentDestination = navBackStackEntry?.destination
-                mainNavigationScreens.forEach { screen ->
-                    BottomNavigationItem(
-                        icon = {
-                            Icon(
-                                screen.icon,
-                                contentDescription = "${screen.resourceId} Icon"
-                            )
-                        },
-                        label = { Text(stringResource(screen.resourceId)) },
-                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                        onClick = {
-                            navController.navigate(screen.route) {
-                                // Pop up to the start destination of the graph to
-                                // avoid building up a large stack of destinations
-                                // on the back stack as users select items
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    saveState = false
+            // Private member of surface
+            // TODO find less hacky way
+            val alpha = ((4.5f * ln(4.dp.value + 1)) + 2f) / 100f
+            val extensionColor = MaterialTheme.colorScheme.surfaceTint.copy(alpha = alpha)
+                .compositeOver(MaterialTheme.colorScheme.surface)
+            Box(modifier = Modifier.background(extensionColor)) {
+                NavigationBar(
+                    modifier = Modifier.windowInsetsPadding(WindowInsets.navigationBars)
+                ) {
+                    val currentDestination = navBackStackEntry?.destination
+                    mainNavigationScreens.forEach { screen ->
+                        NavigationBarItem(
+                            icon = {
+                                Icon(
+                                    screen.icon,
+                                    contentDescription = "${screen.resourceId} Icon"
+                                )
+                            },
+                            label = { Text(stringResource(screen.resourceId)) },
+                            selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                            onClick = {
+                                navController.navigate(screen.route) {
+                                    // Pop up to the start destination of the graph to
+                                    // avoid building up a large stack of destinations
+                                    // on the back stack as users select items
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = false
+                                    }
+                                    // Avoid multiple copies of the same destination when
+                                    // reselecting the same item
+                                    launchSingleTop = true
+                                    // Restore state when reselecting a previously selected item
+                                    restoreState = false
                                 }
-                                // Avoid multiple copies of the same destination when
-                                // reselecting the same item
-                                launchSingleTop = true
-                                // Restore state when reselecting a previously selected item
-                                restoreState = false
                             }
-                        }
-                    )
+                        )
+                    }
                 }
             }
         }
