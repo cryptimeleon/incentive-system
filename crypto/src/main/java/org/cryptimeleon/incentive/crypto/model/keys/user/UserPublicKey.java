11d59802fe5a663a45ff529f837c30c6ec67e1d7
Paul Kramer <pkramer1503@gmail.com>
2022-07-07 20:47:39 +0200

    Deprecate unnecessarily complicated representation constructors of keys

--- crypto/src/main/java/org/cryptimeleon/incentive/crypto/model/keys/user/UserPublicKey.java
+++ crypto/src/main/java/org/cryptimeleon/incentive/crypto/model/keys/user/UserPublicKey.java
@@ -1,15 +1,16 @@
 package org.cryptimeleon.incentive.crypto.model.keys.user;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.NonFinal;
+import org.cryptimeleon.incentive.crypto.model.IncentivePublicParameters;
 import org.cryptimeleon.math.serialization.Representable;
 import org.cryptimeleon.math.serialization.Representation;
 import org.cryptimeleon.math.serialization.annotations.ReprUtil;
 import org.cryptimeleon.math.serialization.annotations.Represented;
 import org.cryptimeleon.math.structures.groups.Group;
 import org.cryptimeleon.math.structures.groups.GroupElement;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.experimental.NonFinal;
 
 @Getter
 @EqualsAndHashCode
@@ -23,9 +24,16 @@ public class UserPublicKey implements Representable {
         this.upk = upk;
     }
 
+    @Deprecated
     public UserPublicKey(Representation repr, Group g1) {
         new ReprUtil(this)
                 .register(g1, "G1")
+                .deserialize(repr);
+    }
+
+    public UserPublicKey(Representation repr, IncentivePublicParameters pp) {
+        new ReprUtil(this)
+                .register(pp.getBg().getG1(), "G1")
                 .deserialize(repr);
     }
 
