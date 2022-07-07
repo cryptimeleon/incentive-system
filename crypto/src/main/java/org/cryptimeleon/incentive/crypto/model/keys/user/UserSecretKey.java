2b88467368f38dc004c0aeefebb29f5388f35609
Paul Kramer <pkramer1503@gmail.com>
2022-07-07 20:47:39 +0200

    Deprecate unnecessarily complicated representation constructors of keys

--- crypto/src/main/java/org/cryptimeleon/incentive/crypto/model/keys/user/UserSecretKey.java
+++ crypto/src/main/java/org/cryptimeleon/incentive/crypto/model/keys/user/UserSecretKey.java
@@ -1,19 +1,21 @@
 package org.cryptimeleon.incentive.crypto.model.keys.user;
 
-import lombok.Value;
-import lombok.experimental.NonFinal;
-import org.cryptimeleon.math.prf.PrfKey;
-import org.cryptimeleon.math.prf.zn.HashThenPrfToZn;
+import org.cryptimeleon.incentive.crypto.model.IncentivePublicParameters;
 import org.cryptimeleon.math.hash.ByteAccumulator;
 import org.cryptimeleon.math.hash.UniqueByteRepresentable;
 import org.cryptimeleon.math.hash.annotations.AnnotatedUbrUtil;
 import org.cryptimeleon.math.hash.annotations.UniqueByteRepresented;
+import org.cryptimeleon.math.prf.PrfKey;
+import org.cryptimeleon.math.prf.zn.HashThenPrfToZn;
 import org.cryptimeleon.math.serialization.Representable;
 import org.cryptimeleon.math.serialization.Representation;
 import org.cryptimeleon.math.serialization.annotations.ReprUtil;
 import org.cryptimeleon.math.serialization.annotations.Represented;
 import org.cryptimeleon.math.structures.rings.zn.Zn;
 import org.cryptimeleon.math.structures.rings.zn.Zn.ZnElement;
+
+import lombok.Value;
+import lombok.experimental.NonFinal;
 
 @Value
 public class UserSecretKey implements Representable, UniqueByteRepresentable {
@@ -32,10 +34,18 @@ public class UserSecretKey implements Representable, UniqueByteRepresentable {
         this.prfKey = prfKey;
     }
 
+    @Deprecated
     public UserSecretKey(Representation repr, Zn zn, HashThenPrfToZn hashThenPrfToZn) {
         new ReprUtil(this)
                 .register(zn, "Zn")
                 .register(hashThenPrfToZn.getLongAesPseudoRandomFunction()::restoreKey, "longAes")
+                .deserialize(repr);
+    }
+
+    public UserSecretKey(Representation repr, IncentivePublicParameters pp) {
+        new ReprUtil(this)
+                .register(pp.getBg().getZn(), "Zn")
+                .register(pp.getPrfToZn().getLongAesPseudoRandomFunction()::restoreKey, "longAes")
                 .deserialize(repr);
     }
 
