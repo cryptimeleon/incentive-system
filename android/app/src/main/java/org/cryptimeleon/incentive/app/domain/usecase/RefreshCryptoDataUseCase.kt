c7880649e8ac3aba043ae9cac655a6156e12f5ac
Paul Kramer <pkramer1503@gmail.com>
2022-07-08 09:45:58 +0200

    Parallelize startup process to half the startup time

--- android/app/src/main/java/org/cryptimeleon/incentive/app/domain/usecase/RefreshCryptoDataUseCase.kt
+++ android/app/src/main/java/org/cryptimeleon/incentive/app/domain/usecase/RefreshCryptoDataUseCase.kt
@@ -1,5 +1,8 @@
 package org.cryptimeleon.incentive.app.domain.usecase
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.first
 import org.cryptimeleon.incentive.app.domain.ICryptoRepository
 import org.cryptimeleon.incentive.app.domain.IPromotionRepository
@@ -17,34 +20,45 @@ class RefreshCryptoDataUseCase(
 
     suspend operator fun invoke() {
         Timber.i("Started")
+        val startTime = System.currentTimeMillis()
 
-        // coroutineScope {
-        //val loadPromotions = async {
-        promotionRepository.reloadPromotions()
-        promotions = promotionRepository.promotions.first()
-        //}
-        //val loadCryptoMaterial = async {
-        currentCryptoMaterial = cryptoRepository.cryptoMaterial.first()
-        // Loads pp and new keys
-        cryptoRepository.refreshCryptoMaterial()
-        updatedCryptoMaterial = cryptoRepository.cryptoMaterial.first()
-        //}
-        //awaitAll(loadCryptoMaterial, loadPromotions)
-        //}
-        // TODO! new promotions
+        coroutineScope {
+            val loadPromotions = async {
+                promotionRepository.reloadPromotions()
+                promotions = promotionRepository.promotions.first()
+            }
+            val loadCryptoMaterial = async {
+                currentCryptoMaterial = cryptoRepository.cryptoMaterial.first()
+                cryptoRepository.refreshCryptoMaterial()
+                updatedCryptoMaterial = cryptoRepository.cryptoMaterial.first()
+            }
+            awaitAll(loadCryptoMaterial, loadPromotions)
+        }
+        // TODO! new promotions?! Maybe only dummy for promotions for which we already have a token!
+        // How about a database method? (e.g. insert without replace)
         if (currentCryptoMaterial == updatedCryptoMaterial && updatedCryptoMaterial != null) {
             // Nothing changed -> Dummy Token
-            promotions.forEach {
-                Timber.i("Start dummy token for $it")
-                cryptoRepository.runIssueJoin(it.promotionParameters, true)
-                Timber.i("Retrieved dummy token for $it")
+            coroutineScope {
+                promotions.map {
+                    async {
+                        Timber.i("Start dummy token for $it")
+                        cryptoRepository.runIssueJoin(it.promotionParameters, true)
+                        Timber.i("Finished dummy token for $it")
+                    }
+                }.awaitAll()
             }
         } else {
-            promotions.forEach {
-                Timber.i("Start token for $it")
-                cryptoRepository.runIssueJoin(it.promotionParameters)
-                Timber.i("Retrieved token for $it")
+            coroutineScope {
+                promotions.map {
+                    async {
+                        Timber.i("Start token for $it")
+                        cryptoRepository.runIssueJoin(it.promotionParameters)
+                        Timber.i("Retrieved token for $it")
+                    }
+                }.awaitAll()
             }
         }
+        val totalMillis = System.currentTimeMillis() - startTime
+        Timber.i("Finished withing " + totalMillis + "miliseconds")
     }
 }
