80e5a35e16914be9b0d97f469b1581704d12346f
Paul Kramer <pkramer1503@gmail.com>
2022-07-18 21:52:15 +0200

    Remove old data classes from DashboardViewModel.kt

--- android/app/src/main/java/org/cryptimeleon/incentive/app/ui/dashboard/DashboardViewModel.kt
+++ android/app/src/main/java/org/cryptimeleon/incentive/app/ui/dashboard/DashboardViewModel.kt
@@ -9,19 +9,6 @@ import org.cryptimeleon.incentive.app.data.CryptoRepository
 import org.cryptimeleon.incentive.app.data.PromotionRepository
 import org.cryptimeleon.incentive.app.domain.usecase.PromotionData
 import org.cryptimeleon.incentive.app.domain.usecase.PromotionInfoUseCase
-import org.cryptimeleon.incentive.crypto.model.Token
-import org.cryptimeleon.incentive.promotion.hazel.HazelPromotion
-import org.cryptimeleon.incentive.promotion.hazel.HazelTokenUpdate
-import org.cryptimeleon.incentive.promotion.streak.RangeProofStreakTokenUpdate
-import org.cryptimeleon.incentive.promotion.streak.SpendStreakTokenUpdate
-import org.cryptimeleon.incentive.promotion.streak.StandardStreakTokenUpdate
-import org.cryptimeleon.incentive.promotion.streak.StreakPromotion
-import org.cryptimeleon.incentive.promotion.vip.ProveVipTokenUpdate
-import org.cryptimeleon.incentive.promotion.vip.UpgradeVipZkpTokenUpdate
-import org.cryptimeleon.incentive.promotion.vip.VipPromotion
-import timber.log.Timber
-import java.math.BigInteger
-import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
@@ -35,67 +22,4 @@ class DashboardViewModel @Inject constructor(
 
     val promotionDataListFlow: Flow<List<PromotionData>> =
         PromotionInfoUseCase(promotionRepository, cryptoRepository, basketRepository).invoke()
-
-    fun onCardClicked(promotionId: BigInteger) {
-        Timber.i("Card with id ${promotionId.toInt()} clicked")
-    }
-
-}
-
-
-sealed class TokenUpdateState {
-    abstract val description: String
-    abstract val sideEffect: String
-}
-
-data class HazelTokenUpdateState(
-    override val description: String,
-    override val sideEffect: String,
-    val current: Int,
-    val goal: Int
-) : TokenUpdateState()
-
-data class VipTokenUpdateState(
-    override val description: String,
-    override val sideEffect: String,
-    val currentStatus: VipStatus,
-    val requiredStatus: VipStatus
-) : TokenUpdateState()
-
-data class UpgradeVipTokenUpdateState(
-    override val description: String,
-    override val sideEffect: String,
-    val currentPoints: Int,
-    val requiredPoints: Int,
-    val vipStatus: VipStatus,
-) : TokenUpdateState()
-
-data class StandardStreakTokenUpdateState(
-    override val description: String,
-    override val sideEffect: String,
-) : TokenUpdateState()
-
-data class RangeProofStreakTokenUpdateState(
-    override val description: String,
-    override val sideEffect: String,
-    val requiredStreak: Int,
-    val currentStreak: Int
-) : TokenUpdateState()
-
-data class SpendStreakTokenUpdateState(
-    override val description: String,
-    override val sideEffect: String,
-    val requiredStreak: Int,
-    val currentStreak: Int
-) : TokenUpdateState()
-
-enum class VipStatus(val statusValue: Int) {
-    NONE(0), // do not delete this!
-    BRONZE(1),
-    SILVER(2),
-    GOLD(3);
-
-    companion object {
-        fun fromInt(value: Int) = values().first { it.statusValue == value }
-    }
 }
