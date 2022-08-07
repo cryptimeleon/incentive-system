e125f246754eca88bff80552832d986eef1e9247
Patrick Schürmann <pschuerm@mail.upb.de>
2022-08-07 14:43:29 +0200

    logs for dsprot integration test, passing

--- services/client/src/test/java/org/cryptimeleon/incentive/client/integrationtest/DsProtectionServiceIntegrationTest.java
+++ services/client/src/test/java/org/cryptimeleon/incentive/client/integrationtest/DsProtectionServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package org.cryptimeleon.incentive.client.integrationtest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.cryptimeleon.incentive.client.DSProtectionClient;
 import org.cryptimeleon.incentive.crypto.model.Transaction;
 import org.cryptimeleon.incentive.crypto.model.TransactionIdentifier;
@@ -13,10 +14,8 @@ import org.cryptimeleon.math.structures.cartesian.Vector;
 /**
  * Tests the double-spending protection service by performing both honest and malicious transactions
  * and checking whether they were correctly recorded in the database.
- *
- * Extends the SpendTest so that we do not have to rewrite all the setup stuff (which would be identical).
- * Setup is done using a BeforeEach-annotated method in the SpendTest class.
  */
+@Slf4j
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DsProtectionServiceIntegrationTest extends TransactionTestPreparation {
     private DSProtectionClient dsProtectionClient;
@@ -45,24 +44,29 @@ public class DsProtectionServiceIntegrationTest extends TransactionTestPreparati
      */
     @Test
     public void honestTransactionTest() {
-        // generate token and basket
+        log.info("Started honest transaction integration test.");
+
+        log.info("Generate token and basket.");
         var token = generateToken(testPromotion.getPromotionParameters(), Vector.of(BigInteger.valueOf(20)));
         var basketId = createBasket();
         assert basketId != null;
 
-        // run Spend-Deduct protocol
+        log.info("Spend-Deduct protocol running...");
         TransactionIdentifier occuredTaId = runSpendDeductWorkflow(token, basketId);
 
-        // query recorded transaction object from database
+        log.info("Done.");
+        log.info("Query recorded transaction from database.");
         String serializedOccuredTaRepr = dsProtectionClient.getTransaction(occuredTaId);
 
-        // deserialize and restore it
+        log.info("Deserialize and restore transaction.");
         JSONConverter jsonConverter = new JSONConverter();
         Representation occuredTaRepr = jsonConverter.deserialize(serializedOccuredTaRepr);
         Transaction occuredTa = new Transaction(occuredTaRepr, cryptoAssets.getPublicParameters());
 
-        // assert that transaction is valid
+        log.info("Verify that transaction is valid.");
         Assertions.assertTrue(occuredTa.getIsValid());
+
+        log.info("Finished honest transaction integration test.");
     }
 
     /**
@@ -71,30 +75,37 @@ public class DsProtectionServiceIntegrationTest extends TransactionTestPreparati
      */
     @Test
     public void doubleSpendingTest() {
-        // generate token and baskets
-        var token = generateToken(testPromotion.getPromotionParameters(), Vector.of(BigInteger.valueOf(42)));
+        log.info("Started double-spending integration test.");
+
+        log.info("Generate token to be double-spended and baskets.");
+        var token = generateToken(testPromotion.getPromotionParameters(), Vector.of(BigInteger.valueOf(20)));
         var basketId1 = createBasket();
         assert basketId1 != null;
         var basketId2 = createBasket();
         assert basketId2 != null;
 
-        // run Spend-Deduct protocol twice with same token
+        log.info("Spend-Deduct protocol running...");
         TransactionIdentifier occuredTaId1 = runSpendDeductWorkflow(token, basketId1);
+        log.info("Done.");
+        log.info("Double-spending Spend-Deduct execution running...");
         TransactionIdentifier occuredTaId2 = runSpendDeductWorkflow(token, basketId2);
+        log.info("Done.");
 
-        // query recorded transaction objects from database
+        log.info("Query recorded transactions from database.");
         String serializedOccuredTaRepr1 = dsProtectionClient.getTransaction(occuredTaId1);
         String serializedOccuredTaRepr2 = dsProtectionClient.getTransaction(occuredTaId2);
 
-        // deserialize and restore them
+        log.info("Deserialize and restore transaction.");
         JSONConverter jsonConverter = new JSONConverter();
         Representation occuredTaRepr1 = jsonConverter.deserialize(serializedOccuredTaRepr1);
         Representation occuredTaRepr2 = jsonConverter.deserialize(serializedOccuredTaRepr2);
         Transaction occuredTa1 = new Transaction(occuredTaRepr1, cryptoAssets.getPublicParameters());
         Transaction occuredTa2 = new Transaction(occuredTaRepr2, cryptoAssets.getPublicParameters());
 
-        // assert that transaction is valid
+        log.info("Verify that double-spending was correctly detected.");
         Assertions.assertTrue(occuredTa1.getIsValid());
         Assertions.assertFalse(occuredTa2.getIsValid());
+
+        log.info("Finished double-spending integration test.");
     }
 }
