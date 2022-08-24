1b14ac31d0f5f0a66b3afeb0136c6adcc4904279
Paul Kramer <pkramer1503@gmail.com>
2022-08-24 12:31:16 +0200

    Remove unused imports

--- services/incentive/src/main/java/org/cryptimeleon/incentive/services/incentive/error/BasketNotPaidException.java
+++ services/incentive/src/main/java/org/cryptimeleon/incentive/services/incentive/error/BasketNotPaidException.java
@@ -1,8 +1,5 @@
 package org.cryptimeleon.incentive.services.incentive.error;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class BasketNotPaidException extends IncentiveServiceException {
     public BasketNotPaidException() {
         super("Basket must be paid to retrieve the results of Earn/Spend!");
--- services/incentive/src/main/java/org/cryptimeleon/incentive/services/incentive/error/OnlineDoubleSpendingException.java
+++ services/incentive/src/main/java/org/cryptimeleon/incentive/services/incentive/error/OnlineDoubleSpendingException.java
@@ -1,8 +1,5 @@
 package org.cryptimeleon.incentive.services.incentive.error;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class OnlineDoubleSpendingException extends IncentiveServiceException {
     public OnlineDoubleSpendingException() {
         super("");
