f8b6414716398f64005c98f81b01f08816c52aaf
Patrick Schürmann <pschuerm@mail.upb.de>
2022-10-10 14:56:55 +0200

    ItemList component (dummy to test import)

--- web/src/components/store/Basket.vue
+++ web/src/components/store/Basket.vue
@@ -1,28 +1,32 @@
 <template>
     <div>
         <h3>ID: {{basket.basketID}}</h3>
+        <ItemList/>
     </div>
 </template>
 
 <script>
+    import ItemList from "./ItemList.vue"
+
     export default {
-        name: 'BasketUI',
-        props: {
-            /*
-            * The object representing the actual basket data.
-            * Consists of:
-            * - basketID: unique identifier of the basket
-            * - items: list of items contained in the basket
-            * - basketItems: list of reward items the user claims for this basket
-            * - paid: flag indicating whether this basket has been paid
-            * - locked: flag indicating whether this basket has been locked 
-            *       (i.e. no more items can be added to it)
-            * - redeemed: flag indicating whether user has already claimed their reward
-            * The paid/locked/redeemed flags alter the color of the div representing the Basket component.
-            */
-            basket: Object
-        }
-    }
+    name: "BasketUI",
+    props: {
+        /*
+        * The object representing the actual basket data.
+        * Consists of:
+        * - basketID: unique identifier of the basket
+        * - items: list of items contained in the basket
+        * - basketItems: list of reward items the user claims for this basket
+        * - paid: flag indicating whether this basket has been paid
+        * - locked: flag indicating whether this basket has been locked
+        *       (i.e. no more items can be added to it)
+        * - redeemed: flag indicating whether user has already claimed their reward
+        * The paid/locked/redeemed flags alter the color of the div representing the Basket component.
+        */
+        basket: Object
+    },
+    components: { ItemList }
+}
 </script>
 
 <style scoped>
---
+++ web/src/components/store/ItemList.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        This is an epic item list.
+    </div>
+</template>
