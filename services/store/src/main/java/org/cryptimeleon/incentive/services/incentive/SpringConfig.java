30fb523a3e50f7123eb8611f3d32fe91de62e7ce
Paul Kramer <paul.kramer@upb.de>
2023-01-12 16:25:10 +0100

    Remove store service since we will include the functionality into the basket service anyway.

--- services/store/src/main/java/org/cryptimeleon/incentive/services/incentive/SpringConfig.java
+++
@@ -1,9 +0,0 @@
-package org.cryptimeleon.incentive.services.incentive;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SpringConfig {
-}
