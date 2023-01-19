fab6c6668980168172231e58a174e3a3f7cec44f
Paul Kramer <paul.kramer@upb.de>
2023-01-19 15:27:41 +0100

    Enable logging at info service

--- services/info/src/main/java/org/cryptimeleon/incentive/services/info/InfoApplication.java
+++ services/info/src/main/java/org/cryptimeleon/incentive/services/info/InfoApplication.java
@@ -2,10 +2,22 @@ package org.cryptimeleon.incentive.services.info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class InfoApplication {
     public static void main(String[] args) {
         SpringApplication.run(InfoApplication.class, args);
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(false);
+        return loggingFilter;
     }
 }
