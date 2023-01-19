4ebd1b7e55efe7e6ef8cd8bc02e3d1edc8affe97
Paul Kramer <paul.kramer@upb.de>
2023-01-19 08:24:57 +0100

    Start adding a simple web ui that shows all user registration coupons

---
+++ web/src/components/ProviderRegistrationTokens.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="registrationCoupon in registrationCoupons">
+      <p>{{registrationCoupon.userInfo}}</p>
+      <p>{{registrationCoupon.userPublicKey}}</p>
+      <p>{{registrationCoupon.userSignature}}</p>
+      <p>{{registrationCoupon.storePublicKey}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProviderRegistrationTokens",
+  data() {
+    return {
+      registrationCoupons: []
+    };
+  },
+  async created() {
+    fetch("/incentive/registration-coupons")
+        .then(response => response.json())
+        .then(data => (this.registrationCoupons = data));
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
