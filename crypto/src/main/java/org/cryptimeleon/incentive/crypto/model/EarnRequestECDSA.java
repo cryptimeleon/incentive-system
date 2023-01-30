7d1276a145d7bc7803c510cedd1404ca272dc048
Paul Kramer <paul.kramer@upb.de>
2023-01-30 15:50:50 +0100

    Make EarnRequestECDSA represented

--- crypto/src/main/java/org/cryptimeleon/incentive/crypto/model/EarnRequestECDSA.java
+++ crypto/src/main/java/org/cryptimeleon/incentive/crypto/model/EarnRequestECDSA.java
@@ -1,18 +1,30 @@
 package org.cryptimeleon.incentive.crypto.model;
 
 import org.cryptimeleon.craco.sig.sps.eq.SPSEQSignature;
+import org.cryptimeleon.math.serialization.ByteArrayRepresentation;
+import org.cryptimeleon.math.serialization.ListRepresentation;
+import org.cryptimeleon.math.serialization.Representable;
+import org.cryptimeleon.math.serialization.Representation;
+import org.cryptimeleon.math.serialization.annotations.Represented;
 import org.cryptimeleon.math.structures.cartesian.Vector;
 import org.cryptimeleon.math.structures.groups.GroupElement;
 
 import java.math.BigInteger;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class EarnRequestECDSA {
+public class EarnRequestECDSA implements Representable {
+    @Represented
     private final BigInteger promotionId;
+    @Represented
     private final Vector<BigInteger> deltaK;
+    @Represented
     private final EarnStoreCoupon earnStoreCoupon;
+    @Represented
     private final SPSEQSignature spseqSignature;
+    @Represented(restorer = "bg")
     private final GroupElement cPrime0;
+    @Represented(restorer = "bg")
     private final GroupElement cPrime1;
 
     public EarnRequestECDSA(BigInteger promotionId, Vector<BigInteger> deltaK, EarnStoreCoupon earnStoreCoupon, SPSEQSignature spseqSignature, GroupElement cPrime0, GroupElement cPrime1) {
@@ -22,6 +34,16 @@ public class EarnRequestECDSA {
         this.spseqSignature = spseqSignature;
         this.cPrime0 = cPrime0;
         this.cPrime1 = cPrime1;
+    }
+
+    public EarnRequestECDSA(Representation representation, IncentivePublicParameters pp) {
+        ListRepresentation listRepresentation = (ListRepresentation) representation;
+        this.promotionId = new BigInteger(((ByteArrayRepresentation) listRepresentation.get(0)).get());
+        this.deltaK = new Vector<>(((ListRepresentation) listRepresentation.get(1)).stream().map(r -> new BigInteger(((ByteArrayRepresentation) r).get())).collect(Collectors.toList()));
+        this.earnStoreCoupon = new EarnStoreCoupon(listRepresentation.get(2));
+        this.spseqSignature = new SPSEQSignature(listRepresentation.get(3), pp.getBg().getG1(), pp.getBg().getG2());
+        this.cPrime0 = pp.getBg().getG1().restoreElement(listRepresentation.get(4));
+        this.cPrime1 = pp.getBg().getG1().restoreElement(listRepresentation.get(5));
     }
 
     public BigInteger getPromotionId() {
@@ -59,5 +81,17 @@ public class EarnRequestECDSA {
     @Override
     public int hashCode() {
         return Objects.hash(promotionId, deltaK, earnStoreCoupon, spseqSignature, cPrime0, cPrime1);
+    }
+
+    @Override
+    public Representation getRepresentation() {
+        return new ListRepresentation(
+                new ByteArrayRepresentation(promotionId.toByteArray()),
+                new ListRepresentation(deltaK.map(bigInteger -> new ByteArrayRepresentation(bigInteger.toByteArray())).stream().collect(Collectors.toList())),
+                earnStoreCoupon.getRepresentation(),
+                spseqSignature.getRepresentation(),
+                cPrime0.getRepresentation(),
+                cPrime1.getRepresentation()
+        );
     }
 }
