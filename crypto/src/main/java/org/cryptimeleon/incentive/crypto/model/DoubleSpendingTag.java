76a3abb49a7ca53b9e26cb429e7864fe788357db
Paul Kramer <paul.kramer@upb.de>
2023-02-03 11:51:48 +0100

    Update dstag to ecdsa version without tracing

--- crypto/src/main/java/org/cryptimeleon/incentive/crypto/model/DoubleSpendingTag.java
+++ crypto/src/main/java/org/cryptimeleon/incentive/crypto/model/DoubleSpendingTag.java
@@ -5,8 +5,6 @@ import org.cryptimeleon.math.serialization.Representation;
 import org.cryptimeleon.math.serialization.annotations.ReprUtil;
 import org.cryptimeleon.math.serialization.annotations.Represented;
 import org.cryptimeleon.math.serialization.converter.JSONConverter;
-import org.cryptimeleon.math.structures.groups.Group;
-import org.cryptimeleon.math.structures.groups.cartesian.GroupElementVector;
 import org.cryptimeleon.math.structures.rings.zn.Zn;
 
 import java.util.Objects;
@@ -19,61 +17,32 @@ import java.util.Objects;
  */
 public class DoubleSpendingTag implements Representable {
     @Represented(restorer = "Zn")
-    private Zn.ZnElement c0; // challenge for deriving the user secret key
-
-    @Represented(restorer = "Zn")
-    private Zn.ZnElement c1; // challenge for deriving the encryption secret key
+    private Zn.ZnElement c; // challenge for deriving the user secret key
 
     @Represented(restorer = "Zn")
     private Zn.ZnElement gamma; // challenge generation helper value
 
-    @Represented(restorer = "Zn")
-    private Zn.ZnElement eskStarProv; // provider share for ElGamal encryption secret key
-
-    @Represented(restorer = "G1")
-    private GroupElementVector ctrace0;
-
-    @Represented(restorer = "G1")
-    private GroupElementVector ctrace1;
-
     public DoubleSpendingTag(Representation repr, IncentivePublicParameters pp) {
-        new ReprUtil(this).register(pp.getBg().getZn(), "Zn").register(pp.getBg().getG1(), "G1").deserialize(repr);
+        new ReprUtil(this).register(pp.getBg().getZn(), "Zn").deserialize(repr);
     }
 
     /**
      * Constructs a double-spending tag from serialized representations of its components.
      */
-    public DoubleSpendingTag(IncentivePublicParameters pp, String serializedC0Repr, String serializedC1Repr, String serializedGammaRepr, String serializedEskProvStarRepr, String serializedCTrace0Repr, String serializedCTrace1Repr) {
+    public DoubleSpendingTag(IncentivePublicParameters pp, String serializedCRepr, String serializedGammaRepr) {
         Zn usedZn = pp.getBg().getZn();
-        Group groupG1 = pp.getBg().getG1();
         JSONConverter jsonConverter = new JSONConverter();
 
-        Representation c0Repr = jsonConverter.deserialize(serializedC0Repr);
-        this.c0 = usedZn.restoreElement(c0Repr);
-
-        Representation c1Repr = jsonConverter.deserialize(serializedC1Repr);
-        this.c1 = usedZn.restoreElement(c1Repr);
+        Representation c0Repr = jsonConverter.deserialize(serializedCRepr);
+        this.c = usedZn.restoreElement(c0Repr);
 
         Representation gammaRepr = jsonConverter.deserialize(serializedGammaRepr);
         this.gamma = usedZn.restoreElement(gammaRepr);
-
-        Representation eskProvRepr = jsonConverter.deserialize(serializedEskProvStarRepr);
-        this.eskStarProv = usedZn.restoreElement(eskProvRepr);
-
-        Representation cTrace0Repr = jsonConverter.deserialize(serializedCTrace0Repr);
-        this.ctrace0 = groupG1.restoreVector(cTrace0Repr);
-
-        Representation cTrace1Repr = jsonConverter.deserialize(serializedCTrace1Repr);
-        this.ctrace1 = groupG1.restoreVector(cTrace1Repr);
     }
 
-    public DoubleSpendingTag(Zn.ZnElement c0, Zn.ZnElement c1, Zn.ZnElement gamma, Zn.ZnElement eskStarProv, GroupElementVector ctrace0, GroupElementVector ctrace1) {
-        this.c0 = c0;
-        this.c1 = c1;
+    public DoubleSpendingTag(Zn.ZnElement c, Zn.ZnElement gamma) {
+        this.c = c;
         this.gamma = gamma;
-        this.eskStarProv = eskStarProv;
-        this.ctrace0 = ctrace0;
-        this.ctrace1 = ctrace1;
     }
 
     @Override
@@ -83,36 +52,16 @@ public class DoubleSpendingTag implements Representable {
 
     @Override
     public String toString() {
-        return this.c0.toString() + " "
-                + this.c1.toString() + " "
-                + this.eskStarProv.toString() + " "
-                + this.gamma.toString() + " "
-                + this.ctrace0.toString() + " "
-                + this.ctrace1.toString();
+        return this.c.toString() + " "
+                + this.gamma.toString();
     }
 
-    public Zn.ZnElement getC0() {
-        return this.c0;
-    }
-
-    public Zn.ZnElement getC1() {
-        return this.c1;
+    public Zn.ZnElement getC() {
+        return c;
     }
 
     public Zn.ZnElement getGamma() {
-        return this.gamma;
-    }
-
-    public Zn.ZnElement getEskStarProv() {
-        return this.eskStarProv;
-    }
-
-    public GroupElementVector getCtrace0() {
-        return this.ctrace0;
-    }
-
-    public GroupElementVector getCtrace1() {
-        return this.ctrace1;
+        return gamma;
     }
 
     @Override
@@ -120,11 +69,11 @@ public class DoubleSpendingTag implements Representable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         DoubleSpendingTag that = (DoubleSpendingTag) o;
-        return Objects.equals(c0, that.c0) && Objects.equals(c1, that.c1) && Objects.equals(gamma, that.gamma) && Objects.equals(eskStarProv, that.eskStarProv) && Objects.equals(ctrace0, that.ctrace0) && Objects.equals(ctrace1, that.ctrace1);
+        return Objects.equals(c, that.c) && Objects.equals(gamma, that.gamma);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(c0, c1, gamma, eskStarProv, ctrace0, ctrace1);
+        return Objects.hash(c, gamma);
     }
 }
