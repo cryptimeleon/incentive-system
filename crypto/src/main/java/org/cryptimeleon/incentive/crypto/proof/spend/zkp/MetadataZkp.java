bb8b6bf8cbdfb63d3750358a4f19789b757e2dec
Paul Kramer <paul.kramer@upb.de>
2023-02-03 12:05:25 +0100

    Update spend proofs to new token format

--- crypto/src/main/java/org/cryptimeleon/incentive/crypto/proof/spend/zkp/MetadataZkp.java
+++ crypto/src/main/java/org/cryptimeleon/incentive/crypto/proof/spend/zkp/MetadataZkp.java
@@ -7,17 +7,15 @@ import org.cryptimeleon.craco.protocols.arguments.sigma.schnorr.DelegateProtocol
 import org.cryptimeleon.craco.protocols.arguments.sigma.schnorr.LinearExponentStatementFragment;
 import org.cryptimeleon.craco.protocols.arguments.sigma.schnorr.LinearStatementFragment;
 import org.cryptimeleon.craco.protocols.arguments.sigma.schnorr.SendThenDelegateFragment;
-import org.cryptimeleon.craco.protocols.arguments.sigma.schnorr.setmembership.SetMembershipFragment;
 import org.cryptimeleon.incentive.crypto.model.IncentivePublicParameters;
 import org.cryptimeleon.incentive.crypto.model.PromotionParameters;
 import org.cryptimeleon.incentive.crypto.model.keys.provider.ProviderPublicKey;
+import org.cryptimeleon.math.expressions.exponent.ExponentExpr;
 import org.cryptimeleon.math.structures.cartesian.ExponentExpressionVector;
 import org.cryptimeleon.math.structures.cartesian.GroupElementExpressionVector;
 import org.cryptimeleon.math.structures.cartesian.Vector;
 import org.cryptimeleon.math.structures.groups.GroupElement;
 import org.cryptimeleon.math.structures.rings.zn.Zn;
-
-import java.math.BigInteger;
 
 /**
  * ZKP to proof knowledge of correct metadata witnesses for a token.
@@ -42,59 +40,33 @@ class MetadataZkp extends DelegateProtocol {
     protected SendThenDelegateFragment.SubprotocolSpec provideSubprotocolSpec(CommonInput pCommonInput, SendThenDelegateFragment.SubprotocolSpecBuilder builder) {
         var commonInput = (SpendDeductZkpCommonInput) pCommonInput;
         var H = new GroupElementExpressionVector(providerPublicKey.getH(pp, promotionParameters).map(GroupElement::expr));
-        var w = pp.getW();
 
         // Variables to use
-        var eskVar = builder.addZnVariable("esk", zn);
         var uskVar = builder.addZnVariable("usk", zn);
-        var dsrnd0Var = builder.addZnVariable("dsrnd0", zn);
-        var dsrnd1Var = builder.addZnVariable("dsrnd1", zn);
-        var dsrndStar0Var = builder.addZnVariable("dsrndStar0", zn);
-        var dsrndStar1Var = builder.addZnVariable("dsrndStar1", zn);
+        var dsidUserStarVar = builder.addZnVariable("dsidUserStar", zn);
+        var dsrndVar = builder.addZnVariable("dsrnd", zn);
+        var dsrndStarVar = builder.addZnVariable("dsrndStar", zn);
         var zVar = builder.addZnVariable("z", zn);
         var tVar = builder.addZnVariable("t", zn);
         var zStarVar = builder.addZnVariable("zStar", zn);
         var tStarVar = builder.addZnVariable("tStar", zn);
         var uStarInverseVar = builder.addZnVariable("uStarInverse", zn);
-        var eskDecVarVector = ExponentExpressionVector.generate(i -> builder.addZnVariable("eskStarUserDec_" + i, zn), pp.getNumEskDigits());
-        var rVector = ExponentExpressionVector.generate(i -> builder.addZnVariable("r_" + i, zn), pp.getNumEskDigits());
         var pointsVector = ExponentExpressionVector.generate(i -> builder.addZnVariable("points_" + i, zn), promotionParameters.getPointsVectorSize());
         var newPointsVector = ExponentExpressionVector.generate(i -> builder.addZnVariable("newPoints_" + i, zn), promotionParameters.getPointsVectorSize());
 
-        // c0=usk*gamma+dsrnd0
-        builder.addSubprotocol("c0=usk*gamma+dsrnd0", new LinearExponentStatementFragment(uskVar.mul(commonInput.gamma).add(dsrnd0Var).isEqualTo(commonInput.c0), zn));
+        // c0=usk*gamma+dsrnd
+        builder.addSubprotocol("c=usk*gamma+dsrnd", new LinearExponentStatementFragment(uskVar.mul(commonInput.gamma).add(dsrndVar).isEqualTo(commonInput.c), zn));
 
-        // c1=esk*gamma+dsrnd1
-        builder.addSubprotocol("c1=esk*gamma+dsrnd1", new LinearExponentStatementFragment(eskVar.mul(commonInput.gamma).add(dsrnd1Var).isEqualTo(commonInput.c1), zn));
-
-        // dsid=w^esk
-        var dsidEskStatement = w.pow(eskVar).isEqualTo(commonInput.dsid);
-        builder.addSubprotocol("dsid=w^esk", new LinearStatementFragment(dsidEskStatement));
-
-        // C=(H.pow(usk, esk, dsrnd_0, dsrnd_1, v, z, t),g_1) split into two subprotocols
-        var commitmentC0Statement = H.innerProduct(ExponentExpressionVector.of(tVar, uskVar, eskVar, dsrnd0Var, dsrnd1Var, zVar).concatenate(pointsVector.map(e -> e))).isEqualTo(commonInput.commitmentC0);
+        // C=(H.pow(usk, dsid, dsrnd, v, z, t),g_1) split into two subprotocols
+        var commitmentC0Statement = H.innerProduct(ExponentExpressionVector.of(tVar, uskVar, commonInput.dsid, dsrndVar, zVar).concatenate(pointsVector.map(e -> e))).isEqualTo(commonInput.commitmentC0);
         builder.addSubprotocol("C0", new LinearStatementFragment(commitmentC0Statement));
         // C1=g is not sent and verified since no witness is involved.
 
-        // C=(H.pow(t^*, usk, \sum_i=0^k[esk^*_(usr,i) * base^i], dsrnd^*_0, dsrnd^*_1, z^*, V-K), g_1^(u^*)) split into two subprotocols
-        // We use the sum to combine the esk^*_usr = \sum proof with the C=.. proof
-        var powersOfEskDecBase = ExponentExpressionVector.generate(i -> pp.getEskDecBase().pow(BigInteger.valueOf(i)).asExponentExpression(), pp.getNumEskDigits()); // construct vector (eskBase^0, eskBase^1, ...)
-        var exponents = new Vector<>(tStarVar, uskVar, eskDecVarVector.innerProduct(powersOfEskDecBase), dsrndStar0Var, dsrndStar1Var, zStarVar).concatenate(newPointsVector);
+        // C=(H.pow(t^*, usk, dsid_user^*, dsrnd^*, z^*, V-K), g_1^(u^*)) split into two subprotocols
+        var exponents = new Vector<ExponentExpr>(tStarVar, uskVar, dsidUserStarVar, dsrndStarVar, zStarVar).concatenate(newPointsVector);
         var cPre0Statement = H.innerProduct(exponents).isEqualTo(commonInput.c0Pre.pow(uStarInverseVar));
         builder.addSubprotocol("C0Pre", new LinearStatementFragment(cPre0Statement));
         builder.addSubprotocol("C1Pre", new LinearStatementFragment(commonInput.c1Pre.pow(uStarInverseVar).isEqualTo(pp.getG1Generator()))); // Use the inverse of uStar to linearize this expression
-
-        // esk^*_(usr,i)\in[0,eskDecBase-1]
-        for (int i = 0; i < pp.getNumEskDigits(); i++) {
-            builder.addSubprotocol("eskDigitSetMembership" + i, new SetMembershipFragment(pp.getEskBaseSetMembershipPublicParameters(), eskDecVarVector.get(i)));
-        }
-
-
-        // ctrace=(w^{r_i} ,{w^{r_i}}^{esk}*w^{esk^*_{usr,i}}) for all i\in[p]
-        for (int i = 0; i < pp.getNumEskDigits(); i++) {
-            builder.addSubprotocol("ctrace0" + i, new LinearStatementFragment(w.pow(rVector.get(i)).isEqualTo(commonInput.ctrace0.get(i))));
-            builder.addSubprotocol("ctrace1" + i, new LinearStatementFragment(w.pow(eskDecVarVector.get(i)).op(commonInput.ctrace0.get(i).pow(eskVar)).isEqualTo(commonInput.ctrace1.get(i))));
-        }
 
         return builder.build();
     }
@@ -104,22 +76,15 @@ class MetadataZkp extends DelegateProtocol {
         var secretInput = (SpendDeductZkpWitnessInput) pSecretInput;
 
         // Add variables to witness
-        builder.putWitnessValue("esk", secretInput.esk);
         builder.putWitnessValue("usk", secretInput.usk);
-        builder.putWitnessValue("dsrnd0", secretInput.dsrnd0);
-        builder.putWitnessValue("dsrnd1", secretInput.dsrnd1);
-        builder.putWitnessValue("dsrndStar0", secretInput.dsrndStar0);
-        builder.putWitnessValue("dsrndStar1", secretInput.dsrndStar1);
+        builder.putWitnessValue("dsidUserStar", secretInput.dsidUserStar);
+        builder.putWitnessValue("dsrnd", secretInput.dsrnd);
+        builder.putWitnessValue("dsrndStar", secretInput.dsrndStar);
         builder.putWitnessValue("z", secretInput.z);
         builder.putWitnessValue("t", secretInput.t);
         builder.putWitnessValue("zStar", secretInput.zStar);
         builder.putWitnessValue("tStar", secretInput.tStar);
         builder.putWitnessValue("uStarInverse", secretInput.uStar.inv());
-
-        for (int i = 0; i < pp.getNumEskDigits(); i++) {
-            builder.putWitnessValue("eskStarUserDec_" + i, (Zn.ZnElement) secretInput.eskStarUserDec.get(i));
-            builder.putWitnessValue("r_" + i, (Zn.ZnElement) secretInput.rVector.get(i));
-        }
 
         for (int i = 0; i < promotionParameters.getPointsVectorSize(); i++) {
             builder.putWitnessValue("points_" + i, (Zn.ZnElement) secretInput.pointsVector.get(i));
--- crypto/src/main/java/org/cryptimeleon/incentive/crypto/proof/spend/zkp/SpendDeductZkpWitnessInput.java
+++ crypto/src/main/java/org/cryptimeleon/incentive/crypto/proof/spend/zkp/SpendDeductZkpWitnessInput.java
@@ -14,32 +14,22 @@ public class SpendDeductZkpWitnessInput implements SecretInput {
     public final Zn.ZnElement t;
     public final Zn.ZnElement tStar;
     public final Zn.ZnElement uStar;
-    public final Zn.ZnElement esk;
-    public final Zn.ZnElement eskStarUser;
-    public final Zn.ZnElement dsrnd0;
-    public final Zn.ZnElement dsrndStar0;
-    public final Zn.ZnElement dsrnd1;
-    public final Zn.ZnElement dsrndStar1;
-    public final RingElementVector eskStarUserDec;
-    public final RingElementVector rVector;
+    public final Zn.ZnElement dsidUserStar;
+    public final Zn.ZnElement dsrnd;
+    public final Zn.ZnElement dsrndStar;
     public final RingElementVector pointsVector;
     public final RingElementVector newPointsVector;
 
-    public SpendDeductZkpWitnessInput(Zn.ZnElement usk, Zn.ZnElement z, Zn.ZnElement zStar, Zn.ZnElement t, Zn.ZnElement tStar, Zn.ZnElement uStar, Zn.ZnElement esk, Zn.ZnElement eskStarUser, Zn.ZnElement dsrnd0, Zn.ZnElement dsrndStar0, Zn.ZnElement dsrnd1, Zn.ZnElement dsrndStar1, RingElementVector eskStarUserDec, RingElementVector rVector, RingElementVector pointsVector, RingElementVector newPointsVector) {
+    public SpendDeductZkpWitnessInput(Zn.ZnElement usk, Zn.ZnElement z, Zn.ZnElement zStar, Zn.ZnElement t, Zn.ZnElement tStar, Zn.ZnElement uStar, Zn.ZnElement dsidUserStar, Zn.ZnElement dsrnd, Zn.ZnElement dsrndStar0, RingElementVector pointsVector, RingElementVector newPointsVector) {
         this.usk = usk;
         this.z = z;
         this.zStar = zStar;
         this.t = t;
         this.tStar = tStar;
         this.uStar = uStar;
-        this.esk = esk;
-        this.eskStarUser = eskStarUser;
-        this.dsrnd0 = dsrnd0;
-        this.dsrndStar0 = dsrndStar0;
-        this.dsrnd1 = dsrnd1;
-        this.dsrndStar1 = dsrndStar1;
-        this.eskStarUserDec = eskStarUserDec;
-        this.rVector = rVector;
+        this.dsidUserStar = dsidUserStar;
+        this.dsrnd = dsrnd;
+        this.dsrndStar = dsrndStar0;
         this.pointsVector = pointsVector;
         this.newPointsVector = newPointsVector;
     }
