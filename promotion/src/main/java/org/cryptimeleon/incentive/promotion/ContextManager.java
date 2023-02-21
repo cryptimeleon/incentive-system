6d8361fede216ec267235ef430bad34418f716c1
Paul Kramer <paul.kramer@upb.de>
2023-02-21 13:27:43 +0100

    Add documentation of context manager.

--- promotion/src/main/java/org/cryptimeleon/incentive/promotion/ContextManager.java
+++ promotion/src/main/java/org/cryptimeleon/incentive/promotion/ContextManager.java
@@ -11,6 +11,16 @@ import java.math.BigInteger;
 import java.util.UUID;
 
 public class ContextManager {
+
+    /**
+     * Function that computes the context hash, a hash that captures the whole context of a spend transaction to prevent
+     * attacks where something is altered in a retry.
+     *
+     * @param tokenUpdateId the id of the token update of this spend transaction
+     * @param basketValue   the value of the corresponding basket
+     * @param metadata      the metadata object for this update / the promotion
+     * @return a hash for checking integrity of these parameters
+     */
     public static UniqueByteRepresentable computeContext(UUID tokenUpdateId, Vector<BigInteger> basketValue, ZkpTokenUpdateMetadata metadata) {
         SHA256HashFunction sha256HashFunction = new SHA256HashFunction();
         JSONConverter jsonConverter = new JSONConverter();
