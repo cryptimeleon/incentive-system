c29bc481627e33b1016815f4d64f84754b74de86
Paul Kramer <paul.kramer@upb.de>
2023-03-21 17:22:55 +0100

    Add host environment variable to deploy script and cleanup treafik ports

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,4 +7,7 @@ export BASKET_PROVIDER_SECRET=$(./util.sh)
 export BASKET_PROVIDER_SECRET_TWO=$(./util.sh)
 export INCENTIVE_PROVIDER_SECRET=$(./util.sh)
 export STORE_SHARED_SECRET=$(./util.sh)
+
+export HOST=incentives.cs.uni-paderborn.de
+
 docker-compose -f docker-compose-nginx.yaml up -d
