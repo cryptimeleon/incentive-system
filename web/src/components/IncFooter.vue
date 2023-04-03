de0239b51ccf39e70a8a306ea554a62ecda445ad
Paul Kramer <paul.kramer@upb.de>
2023-04-03 16:38:02 +0200

    Add impressum for cryptimeleon deployment

--- web/src/components/IncFooter.vue
+++ web/src/components/IncFooter.vue
@@ -2,7 +2,8 @@
   <footer class="bg-gray-900 text-white">
     <div class="text-center p-4">
       2022 Cryptimeleon |
-      <router-link to="/privacy-policy" class="text-white hover:underline">Privacy Policy</router-link>
+      <router-link to="/privacy-policy" class="text-white hover:underline">Privacy Policy</router-link> |
+      <router-link to="/impressum" class="text-white hover:underline">Impressum</router-link>
     </div>
   </footer>
 </template>
