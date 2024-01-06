a8e9c8830974b3705a2bca80a092ccfc102f6b90
Jan Bobolz <mail@jan-bobolz.de>
2024-01-06 19:17:49 +0000

    New URL

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -8,7 +8,7 @@ export BASKET_PROVIDER_SECRET_TWO=$(./util.sh)
 export INCENTIVE_PROVIDER_SECRET=$(./util.sh)
 export STORE_SHARED_SECRET=$(./util.sh)
 
-export HOST=incentives.cs.uni-paderborn.de
+export HOST=incentimeleon.cryptimeleon.org
 # Use this for local deployments or change the host to your server's url
 # export HOST=localhost:8009
 
